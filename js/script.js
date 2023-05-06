7cd607427d46298554f3e905c9e6976fd3ef00c6
Mandy PB <plaisance.mandy@gmail.com>
2023-05-06 00:31:01 -0400

    Needed to adjust JS code to make drop-down list able to change CSS

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@ var body = document.querySelector("body");
 select.addEventListener("change", function (e) {
   var mood = e.target.value;
   if (mood === "happy") {
-    body.classList.remove("body.sad");
-    body.classList.remove("body.passionate");
-    body.classList.add("body.happy");
+    body.classList.remove("sad");
+    body.classList.remove("passionate");
+    body.classList.add("happy");
   } else if (mood === "sad") {
-    body.classList.remove("body.happy");
-    body.classList.remove("body.passionate");
-    body.classList.add("body.sad");
+    body.classList.remove("happy");
+    body.classList.remove("passionate");
+    body.classList.add("sad");
   } else if (mood === "passionate") {
-    body.classList.remove("body.sad");
-    body.classList.remove("body.happy");
-    body.classList.add("body.passionate");
+    body.classList.remove("sad");
+    body.classList.remove("happy");
+    body.classList.add("passionate");
   }
 });
